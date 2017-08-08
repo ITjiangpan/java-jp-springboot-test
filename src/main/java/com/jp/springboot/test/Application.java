83e940681e4f17ff03b2cb2f8ff24def68eecbd6
jiangpan <jiangpan@zbj.com>
2017-08-08 17:04:04 +0800

    初始化

---
+++ src/main/java/com/jp/springboot/test/Application.java
@@ -0,0 +1,27 @@
+package com.jp.springboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**     
+ *
+ * @author jiangpan 
+ * @title Application.java
+ * @date 2017年8月8日 
+ *     
+ */
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@RequestMapping("/")
+	public String toIndex(){
+		return "HELLO WORLD";
+	}
+	@RequestMapping("/")
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
